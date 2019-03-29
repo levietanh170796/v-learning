a175957ef035776ddd59c268b6d5ee7470b4503b
noidz <noida@nal.vn>
2019-03-29 17:43:37 +0700

    add theme admin

---
+++ resources/lang/vendor/adminlte/en/adminlte.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Họ và tên',
+    'email'                       => 'Email',
+    'password'                    => 'Mật khẩu',
+    'retype_password'             => 'Nhập lại mật khẩu',
+    'remember_me'                 => 'Ghi nhớ mật khẩu',
+    'register'                    => 'Đăng ký',
+    'register_a_new_membership'   => 'Đăng kỳ tài khoản mới',
+    'i_forgot_my_password'        => 'Tôi đã quên mật khẩu',
+    'i_already_have_a_membership' => 'Tôi đã có tài khoản',
+    'sign_in'                     => 'Đăng nhập',
+    'log_out'                     => 'Đăng xuất',
+    'toggle_navigation'           => 'Toggle navigation',
+    'login_message'               => 'Đăng nhập phiên làm việc mới',
+    'register_message'            => 'Đăng ký thành viên mới',
+    'password_reset_message'      => 'Đổi mật khẩu',
+    'reset_password'              => 'Đổi mật khẩu',
+    'send_password_reset_link'    => 'Gửi link đổi mật khẩu',
+];
